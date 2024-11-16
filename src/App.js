d2b4030de34e53accc7559671b3be17c2cb0da04
Kamgame25 <kamgame652@gmail.com>
2024-11-16 03:55:33 +0100

    work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import ButtonGroup from '@mui/material/ButtonGroup';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+import TextField from '@mui/material/TextField';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+
+      <Box
+          sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              w: '100%',
+              bgcolor:'#ffcc80',
+              borderRadius:'30px',
+              mx:'40%',
+
+              // width:'100%',
+              py:'10px',
+              my: '50px'
+              // '& > *': {
+              //   m: 1,
+              // },
+          }}>
+          <h2>Zaloguj się aby kontynować</h2>
+          <TextField
+              required
+              id="outlined-required"
+              label="E-Mail lub login"
+              sx={{my: '20px'}}
+          />
+          <TextField
+              required
+              id="outlined-required"
+              label="Hasło"
+          />
+
+          <Button sx={{
+              my: '20px'
+          }} variant="contained">Zaloguj się</Button>
+          <FormGroup>
+              <FormControlLabel control={<Switch defaultChecked/>} label="Pozostać zalogowanym?"/>
+          </FormGroup>
+
+          <ButtonGroup variant="text" aria-label="Basic button group" >
+              <Button>Google</Button>
+              <Button>Facebook</Button>
+              <Button>Github</Button>
+          </ButtonGroup>
+
+      </Box>
+
+
+  </>
+
   );
 }
 
